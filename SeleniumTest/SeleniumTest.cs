9b8f1d54b840cc51450a9ac5ef6b57db650b05f9
Dawid Biela <dawid.biela93@gmail.com>
2017-07-18 22:29:42 +0200

    Add project files.

---
+++ SeleniumTest/SeleniumTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumTest
+{
+    class SeleniumTest
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http:www.wykop.pl");
+            IWebElement search = driver.FindElement(By.Name("nsQ"));
+        }
+    }
+}
